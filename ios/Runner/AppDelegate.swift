dd13dae71a6d0ac0da3d6a6001abc15a92354515
Bruno Bernardino <me@brunobernardino.com>
2024-04-13 19:36:39 +0100

    Fix auto-upload for iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,9 +9,14 @@ import workmanager
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    WorkmanagerPlugin.registerBGProcessingTask(withIdentifier: "auto-upload-photos")
+    
+    WorkmanagerPlugin.registerTask(withIdentifier: "auto-upload-photos")
+    WorkmanagerPlugin.registerTask(withIdentifier: "com.bewcloud.mobile.iOSBackgroundAppRefresh")
+    WorkmanagerPlugin.registerTask(withIdentifier: "workmanager.background.task")
+      
+    // WorkmanagerPlugin.registerBGProcessingTask(withIdentifier: "auto-upload-photos")
     // Run background sync every 2 hours
-    WorkmanagerPlugin.registerPeriodicTask(withIdentifier: "com.bewcloud.mobile.iOSBackgroundAppRefresh", frequency: NSNumber(value: 2 * 60 * 60))
+    // WorkmanagerPlugin.registerPeriodicTask(withIdentifier: "com.bewcloud.mobile.iOSBackgroundAppRefresh", frequency: NSNumber(value: 2 * 60 * 60))
     UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(2 * 60 * 60))
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
